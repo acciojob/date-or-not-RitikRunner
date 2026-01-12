1a8e8ad85cf1a4210e66349470a13f71c1c71936
Ritik Sharma <ritiksharma10508@gmail.com>
2026-01-12 12:22:19 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 var isDate = function (input) {
   //   write your code here
+	return !isNan(date.parse(input))
+	
 };
 
 // Do not change the code below.
-const input = prompt("Enter Date.");
+const input = prompt("Enter Date."); 
 alert(isDate(input));
